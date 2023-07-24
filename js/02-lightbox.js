d4ebe33575c4e3f793028b7ae8cb1d3c38accfe8
Xenomine09 <maks.optim@gmail.com>
2023-07-24 15:38:25 +0200

    fixes and task 2 start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (element) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href=${element.original}>
+        <img
+          class="gallery__image"
+          src=${element.preview}
+          alt=${element.description}
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
